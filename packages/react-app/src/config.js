19f36885c6ee25c619ca781296d93b12e027bafa
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-08-26 01:53:16 +0530

    fix typescript bugs

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,7 +1,7 @@
 export const CHAIN_IDS = {
   1337: 'Ganache Local',
-  42: 'Kovan'
-}
+  42: 'Kovan',
+};
 
 const ENVS = {
   DEV: {
@@ -10,16 +10,18 @@ const ENVS = {
     chainEndpoint: process.env.REACT_APP_LOCAL_CHAIN_ENDPOINT,
     algernonAddress: '0x26950f5d5B2eA2AC11d8A2cB67a62D8831Be3d8F',
     algerTokenAddress: '0x98012ed413Bc79B2A2fc1420FDC90670aAb96B61',
-    theGraphNetworkSubgraph: 'https://api.thegraph.com/subgraphs/name/ministry-of-decentralization/graph-network-rinkby'
+    theGraphNetworkSubgraph:
+      'https://api.thegraph.com/subgraphs/name/ministry-of-decentralization/graph-network-rinkby',
   },
-  RINKEBY: {
+  KOVAN: {
     chainId: 42,
     networkName: 'Kovan',
     chainEndpoint: process.env.REACT_APP_KOVAN_CHAIN_ENDPOINT,
     algernonAddress: '0xE494E00411D49cfb6466391c5aA0166505fEe6B7',
     algerTokenAddress: '0xDC904045490Fd90415B3299373A4741B15393fD5',
-    theGraphNetworkSubgraph: 'https://api.thegraph.com/subgraphs/name/ministry-of-decentralization/graph-network-rinkby'
-  }
-}
+    theGraphNetworkSubgraph:
+      'https://api.thegraph.com/subgraphs/name/ministry-of-decentralization/graph-network-rinkby',
+  },
+};
 
-export default ENVS[process.env.REACT_APP_DESTAKE_ENV]
+export default ENVS[process.env.REACT_APP_DESTAKE_ENV];
--- packages/react-app/src/selectors/graphAccount.ts
+++ packages/react-app/src/selectors/graphAccount.ts
@@ -1,35 +1,57 @@
+import { createSelector } from 'reselect';
+import { nativeToDisplayBalance } from '../utils/formatters';
+import {
+  CuratorTotals,
+  CuratorTotalsView,
+  DelegatorTotals,
+  DelegatorTotalsView,
+  GraphAccount,
+  GraphAccountView,
+} from '../types/theGraphProtocol';
+import { convertToChecksum } from '../utils/helpers';
 
-import { createSelector } from 'reselect'
-import { nativeToDisplayBalance } from '../utils/formatters'
-import { GraphAccount, GraphAccountView } from '../types/theGraphProtocol'
-import { convertToChecksum } from '../utils/helpers'
+const formatDelegatorTotals = (
+  totals: DelegatorTotals | null
+): DelegatorTotalsView | null => {
+  return totals != null
+    ? {
+        ...totals,
+        totalStakedTokensDisplay: nativeToDisplayBalance(
+          totals.totalStakedTokens
+        ),
+        totalUnstakedTokensDisplay: nativeToDisplayBalance(
+          totals.totalUnstakedTokens
+        ),
+        totalRealizedRewardsDisplay: nativeToDisplayBalance(
+          totals.totalRealizedRewards
+        ),
+      }
+    : null;
+};
 
-const formatDelegatorTotals = (totals: DelegatorTotals): DelegatorTotalsView => {
-  return totals != null ? 
-    {
-      ...totals,
-      totalStakedTokensDisplay: nativeToDisplayBalance(totals.totalStakedTokens),
-      totalUnstakedTokensDisplay: nativeToDisplayBalance(totals.totalUnstakedTokens),
-      totalRealizedRewardsDisplay: nativeToDisplayBalance(totals.totalRealizedRewards)
-    }
-    :
-    null
-}
-
-const formatCuratorTotals = (totals: CuratorTotals): CuratorTotalsView => {
-  return totals !== null ? 
-    {
-      ...totals,
-      totalSignalledTokensDisplay: nativeToDisplayBalance(totals.totalSignalledTokens),
-      totalUnsignalledTokensDisplay: nativeToDisplayBalance(totals.totalUnsignalledTokens),
-      totalWithdrawnTokensDisplay: nativeToDisplayBalance(totals.totalWithdrawnTokens),
-      realizedRewardsDisplay: nativeToDisplayBalance(totals.realizedRewards),
-      annualizedReturnDisplay: nativeToDisplayBalance(totals.annualizedReturn),
-      totalReturnDisplay: nativeToDisplayBalance(totals.totalReturn)
-    }
-    :
-    null
-}
+const formatCuratorTotals = (
+  totals: CuratorTotals | null
+): CuratorTotalsView | null => {
+  return totals !== null
+    ? {
+        ...totals,
+        totalSignalledTokensDisplay: nativeToDisplayBalance(
+          totals.totalSignalledTokens
+        ),
+        totalUnsignalledTokensDisplay: nativeToDisplayBalance(
+          totals.totalUnsignalledTokens
+        ),
+        totalWithdrawnTokensDisplay: nativeToDisplayBalance(
+          totals.totalWithdrawnTokens
+        ),
+        realizedRewardsDisplay: nativeToDisplayBalance(totals.realizedRewards),
+        annualizedReturnDisplay: nativeToDisplayBalance(
+          totals.annualizedReturn
+        ),
+        totalReturnDisplay: nativeToDisplayBalance(totals.totalReturn),
+      }
+    : null;
+};
 
 const formatGraphAccount = (account: GraphAccount): GraphAccountView => {
   return {
@@ -40,14 +62,13 @@ const formatGraphAccount = (account: GraphAccount): GraphAccountView => {
     stakingApprovalDisplay: nativeToDisplayBalance(account.stakingApproval),
     gnsApprovalDisplay: nativeToDisplayBalance(account.gnsApproval),
     delegator: formatDelegatorTotals(account.delegator),
-    curator: formatCuratorTotals(account.curator)
-  }
-}
+    curator: formatCuratorTotals(account.curator),
+  };
+};
 
 export const selectGraphAccount = createSelector(
   [(a: GraphAccount): GraphAccount => a],
   (account: GraphAccount): GraphAccountView => {
-    return formatGraphAccount(account) as GraphAccountView
+    return formatGraphAccount(account) as GraphAccountView;
   }
-)
-
+);
